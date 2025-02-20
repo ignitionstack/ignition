b1cb6da358b38be9b31ad9493e4fe827ba7fe4fc
pwnintended <92651319+pwnintended@users.noreply.github.com>
2025-02-20 19:34:25 +0100

    chore: changes to ci lint (#5)
    * chore: changes to ci
    
    * chore: gofmt

--- pkg/engine/errors.go
+++ pkg/engine/errors.go
@@ -3,24 +3,24 @@ package engine
 import "net/http"
 
 type RequestError struct {
-    Message    string
-    StatusCode int
+	Message    string
+	StatusCode int
 }
 
 func (e RequestError) Error() string {
-    return e.Message
+	return e.Message
 }
 
 func NewBadRequestError(message string) RequestError {
-    return RequestError{
-        Message:    message,
-        StatusCode: http.StatusBadRequest,
-    }
+	return RequestError{
+		Message:    message,
+		StatusCode: http.StatusBadRequest,
+	}
 }
 
 func NewNotFoundError(message string) RequestError {
-    return RequestError{
-        Message:    message,
-        StatusCode: http.StatusNotFound,
-    }
+	return RequestError{
+		Message:    message,
+		StatusCode: http.StatusNotFound,
+	}
 }
